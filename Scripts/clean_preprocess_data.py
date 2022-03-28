70eba44a3478ec3a8b0a0d2b6ea804926cd3b49c
Camila Gaitán Mosquera <camila.gaitanm@gmail.com>
2022-03-29 01:05:51 +0200

    Add training data

---
+++ Scripts/clean_preprocess_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:386cc143de76e8b61de8bd3d58c7876c72be84a1f5ad7250a9cfadaf4ef8785a
+size 1434
---
+++ Scripts/constants.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b769ed1b1f50f4d2962cc4d46d6e501ada4bda6f38aeade37d5ab80809f6659
+size 258

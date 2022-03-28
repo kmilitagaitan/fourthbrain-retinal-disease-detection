bdac2ce800b553a6a9ee475d7939c3f75c0c6a42
Camila Gaitán Mosquera <camila.gaitanm@gmail.com>
2022-03-29 01:54:58 +0200

    Update notebook with label propagation feature

--- Scripts/constants.py
+++ Scripts/constants.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b769ed1b1f50f4d2962cc4d46d6e501ada4bda6f38aeade37d5ab80809f6659
-size 258
+oid sha256:deb146a125f2053fad60877207ea9c49702c5116fdc599d167578da129700cbd
+size 358
